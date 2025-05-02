bf9bf758bbba31306009404057e9ca3edf06e095
Abdul Junaid <abduljunaid02@gmail.com>
2025-05-02 11:37:43 +0200

    Solved: What's Your Name

---
+++ WhatsYourName.py
@@ -0,0 +1,7 @@
+def print_full_name(first, last):
+    return print(f"Hello {first} {last}! You just delved into python.")
+
+if __name__ == "__main__":
+    first = input()
+    last = input()
+    print_full_name(first, last)
