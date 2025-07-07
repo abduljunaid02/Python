41d3c1b2870280ff98b7dfeff7958b9d0cfd7bd9
Abdul Junaid <abduljunaid02@gmail.com>
2025-07-07 16:12:07 +0200

    Solved: arrayMath

---
+++ arrayMath.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+dim = tuple(map(int, input().split()))
+Adata = list(map(int, input().split()))
+A = np.array(Adata).reshape(dim)
+Bdata = list(map(int, input().split()))
+B = np.array(Bdata).reshape(dim)
+
+
+
+print(np.add(A,B))
+print(np.subtract(A,B))
+print(np.multiply(A,B))
+print(np.divide(A,B))
+print(np.mod(A,B))
+print(np.power(A,B))
